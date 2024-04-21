9dc08d5931f7d4eb89ac1b1869505c4017d212a6
spiderbot <joshipiyush217@gmail.com>
2024-04-21 12:06:57 +0530

    example for forkjointask

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ForkJoinPool pool = new ForkJoinPool();
         ForkJoinTask task = new ForkJoinTask(0,600);
+        // learn about invoke(), submit(), execute() and their differences
         Integer result = pool.invoke(task);
         System.out.println("Demo Complete with result " + result);
     }
